1ed8261116002015081969f1ca8b180902beb09e
PiotrO9 <piotr.ostrowski02@wp.pl>
2024-11-17 17:09:37 +0100

    Add friendships table and endpnint for fetching that datas

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -4,5 +4,6 @@ const chatController = require('../controlers/chatController');
 const router = express.Router();
 
 router.post('/getChats', chatController.findUserChat);
+router.post('/getFriends', chatController.getFriendsOfGivenUser);
 
 module.exports = router;
