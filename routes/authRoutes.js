f95d71e9d52848eec5eba1b0544b0e5d848439c2
PiotrO9 <piotr.ostrowski02@wp.pl>
2024-11-23 20:56:34 +0100

    Add swagger configuration and endpoint description

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,8 +3,133 @@ const authController = require('../controlers/authController');
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Autoryzacja
+ *   description: Endpointy zarządzające logowaniem, wylogowaniem i sesją użytkownika
+ */
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Logowanie użytkownika
+ *     tags: [Autoryzacja]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: Email użytkownika
+ *               password:
+ *                 type: string
+ *                 description: Hasło użytkownika
+ *     responses:
+ *       200:
+ *         description: Zalogowano pomyślnie, zwraca dane użytkownika
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 userId:
+ *                   type: string
+ *                   description: Unikalne ID użytkownika
+ *                 name:
+ *                   type: string
+ *                   description: Imię użytkownika
+ *                 email:
+ *                   type: string
+ *                   description: Email użytkownika
+ *                 password:
+ *                   type: string
+ *                   description: Haslo użytkownika
+ *                 avatar:
+ *                   type: string
+ *                   format: url
+ *                   description: URL do avatara użytkownika
+ *                 isOnline:
+ *                   type: boolean
+ *                   description: Status użytkownika (czy jest online)
+ *                 createdAt:
+ *                   type: string
+ *                   format: date-time
+ *                   description: Data utworzenia konta
+ *       401:
+ *         description: Niepoprawne dane logowania (email lub hasło)
+ */
+
 router.post('/login', authController.loginUser);
+
+/**
+ * @swagger
+ * /logout:
+ *   post:
+ *     summary: Wyloguj użytkownika
+ *     tags: [Autoryzacja]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userId:
+ *                 type: string
+ *                 description: ID użytkownika
+ *     responses:
+ *       200:
+ *         description: Wylogowano pomyślnie
+ *       401:
+ *         description: Nieprawidłowe zapytanie lub użytkownik nie znaleziony
+ */
 router.post('/logout', authController.logoutUser);
+
+/**
+ * @swagger
+ * /user:
+ *   get:
+ *     summary: Pobierz dane użytkownika
+ *     tags: [Użytkownicy]
+ *     responses:
+ *       200:
+ *         description: Dane użytkownika
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 userId:
+ *                   type: string
+ *                   description: Unikalne ID użytkownika
+ *                 name:
+ *                   type: string
+ *                   description: Imię użytkownika
+ *                 email:
+ *                   type: string
+ *                   description: Adres email użytkownika
+ *                 password:
+ *                   type: string
+ *                   description: Hasło użytkownika
+ *                 avatar:
+ *                   type: string
+ *                   format: url
+ *                   description: URL do avatara użytkownika
+ *                 isOnline:
+ *                   type: boolean
+ *                   description: Status online użytkownika
+ *                 createdAt:
+ *                   type: string
+ *                   format: date-time
+ *                   description: Data utworzenia konta użytkownika
+ *       401:
+ *         description: Brak autoryzacji użytkownika
+ */
 
 router.post('/user', authController.getUserById);
 
---
+++ swaggerConfig.js
@@ -0,0 +1,23 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Express API Documentation',
+      version: '1.0.0',
+      description: 'API documentation for my Express.js backend',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Local server',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
